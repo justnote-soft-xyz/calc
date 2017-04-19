03b76f6cbb0ef9b4d9b4c866f6ac730c665f7eb0
Just Note <soft.justnote@gmail.com>
2017-04-19 15:08:16 +0300

    Создаем цыклы и основные события

--- javascript/main.js
+++ javascript/main.js
@@ -4,10 +4,27 @@ decimalBtn = document.getElementById('decimal'),
     result = document.getElementById('result'),
      clearbtn = document.getElementById('clearbtn'); 
  
-console.log(decimal);
-console.log(result);
-console.log(clearbtn);
-
+for (var i=0; i<numbers.length; i++){
+    var number = numbers[i];
+    number.addEventListener('click', function (e) {
+        console.log('клик по кнопке с номером');
+});
+};
+for (var i=0; i<operations.length; i++){
+    var operation = operations[i];
+    operation.addEventListener('click', function (e) {
+        console.log('клик по кнопке с операцией');
+});
+};
+clearbtn.addEventListener('click', function (e) {
+     console.log('клик по кнопке с Очистить');
+});
+result.addEventListener('click', function (e) {
+     console.log('клик по кнопке с результат');
+});
+decimalBtn.addEventListener('click', function (e) {
+     console.log('клик по кнопке с точкой');
+});
 /*
 function namberPress()  {
     // ввод значений
